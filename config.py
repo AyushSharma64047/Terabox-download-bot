01608897149f1b16ae390b6d0ad7056d46c38318
Aayush64047Developer <106085120+AyushSharma64047@users.noreply.github.com>
2023-04-12 19:56:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 
 # Telegram bot token
-TELEGRAM_TOKEN = "5647123835:AAHN6PUFsVpCFkxYuSdHlX4iW_Wc8Nuo7IU"
+TELEGRAM_TOKEN = "5808002899:AAENcdAaNYwSvD0-XbGRSAk-rPg96M5_zBc"
 
 #terabox
 TERABOX_EMAIL = "dudupudu2@gmail.com"
